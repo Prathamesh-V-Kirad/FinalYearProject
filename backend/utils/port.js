60c91fceeee41e55044b02dddaa2029ba72a84fc
Prathamesh-V-Kirad <kiradvprathamesh2002@gamil.com>
2024-04-03 19:21:43 +0530

    add port.js file

---
+++ backend/utils/port.js
@@ -0,0 +1,22 @@
+const MIN_PORT = 20000;
+const MAX_PORT = 30000;
+
+const takenPortSet = new Set();
+
+const getPort = async () => {
+  let port = getRandomPort();
+
+  while(takenPortSet.has(port)) {
+    port = getRandomPort();
+  }
+
+  takenPortSet.add(port);
+
+  return port;
+};
+
+const releasePort = (port) => takenPortSet.delete(port);
+
+const getRandomPort = () => Math.floor(Math.random() * (MAX_PORT - MIN_PORT + 1) + MIN_PORT); 
+
+export {getPort , releasePort}
